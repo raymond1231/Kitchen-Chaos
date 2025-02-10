04a30297cdfe29c36b18b4fcb88eb3044d4a86d8
Raymond <88035771+Youcha12345@users.noreply.github.com>
2025-02-10 11:39:47 +0800

    Updates

--- Assets/AssetsRaymond/ScriptsRaymond/GameInput.cs
+++ Assets/AssetsRaymond/ScriptsRaymond/GameInput.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+
+    public event EventHandler OnInteractAction;
+
+
     private PlayerInputActions playerInputActions;
 
 
@@ -11,6 +16,13 @@ public class GameInput : MonoBehaviour
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {     
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalized()
