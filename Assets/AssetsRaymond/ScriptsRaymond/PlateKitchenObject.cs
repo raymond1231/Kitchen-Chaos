80f2436af97a886a35fd958fb28256a4ba2cd59d
Raymond <88035771+Youcha12345@users.noreply.github.com>
2025-02-14 02:35:02 +0800

    Updates

--- Assets/AssetsRaymond/ScriptsRaymond/PlateKitchenObject.cs
+++ Assets/AssetsRaymond/ScriptsRaymond/PlateKitchenObject.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs 
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+    [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
+
     private List<KitchenObjectSO> kitchenObjectSOList;
 
     private void Awake()
@@ -11,8 +19,33 @@ public class PlateKitchenObject : KitchenObject
         kitchenObjectSOList = new List<KitchenObjectSO>();
     }
 
-    public void AddIngredient(KitchenObjectSO kitchenObjectSO)
+    public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
-        kitchenObjectSOList.Add(kitchenObjectSO);
+        if(!validKitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            // Not a valid ingredient
+            return false;
+        }
+        if(kitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            // Already has this type
+            return false;
+        }
+        else
+        {
+            kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
+
+            return true;
+        }
+    }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
     }
 }
