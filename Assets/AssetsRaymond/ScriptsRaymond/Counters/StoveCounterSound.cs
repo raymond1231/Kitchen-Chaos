0870c7ba65413be5884e2c1395ed878e102f8fc8
Raymond <88035771+Youcha12345@users.noreply.github.com>
2025-02-14 17:25:50 +0800

    Updates

---
+++ Assets/AssetsRaymond/ScriptsRaymond/Counters/StoveCounterSound.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCounterSound : MonoBehaviour
+{
+    [SerializeField] private StoveCounter stoveCounter;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+}
--- Assets/AssetsRaymond/ScriptsRaymond/Player.cs
+++ Assets/AssetsRaymond/ScriptsRaymond/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
     public static Player Instance { get; set; }
 
 
-
+    public event EventHandler OnPickedSomething;
     public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
     public class OnSelectedCounterChangedEventArgs : EventArgs
     {
@@ -175,6 +175,11 @@ public class Player : MonoBehaviour, IKitchenObjectParent
     public void SetKitchenObject(KitchenObject kitchenObject)
     {
         this.kitchenObject = kitchenObject;
+
+        if(kitchenObject != null)
+        {
+            OnPickedSomething?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     public KitchenObject GetKitchenObject()
